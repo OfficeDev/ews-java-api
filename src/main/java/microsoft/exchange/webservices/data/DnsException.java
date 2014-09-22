a939028d8d54183b8b38d0d01d8fcd0785b671d0
Victor Boctor <vboctor@microsoft.com>
2014-09-22 15:54:41 -0700

    DnsException should pass message to parent and remove getErrorCode()

--- src/main/java/microsoft/exchange/webservices/data/DnsException.java
+++ src/main/java/microsoft/exchange/webservices/data/DnsException.java
@@ -11,30 +11,16 @@ package microsoft.exchange.webservices.data;
  * Defines DnsException class.
  */
 class DnsException extends Exception {
-
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
-	/** The exception. */
-	String exception;
-
 	/**
 	 * Instantiates a new dns exception.
-	 * 
+	 *
 	 * @param exceptionMessage
 	 *            the exception message
 	 */
 	protected DnsException(String exceptionMessage) {
-		exception = exceptionMessage;
-
-	}
-
-	/**
-	 * Gets the error.
-	 * 
-	 * @return the error
-	 */
-	protected String getError() {
-		return exception;
+		super(exceptionMessage);
 	}
 }
