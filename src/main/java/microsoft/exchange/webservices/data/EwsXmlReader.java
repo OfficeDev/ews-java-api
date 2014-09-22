a8b06acf9359203d37986e88c30a753ed3804776
Victor Boctor <vboctor@microsoft.com>
2014-09-21 20:32:54 -0700

    Fix #33: EwsXmlReader uses == to compare Objects

--- src/main/java/microsoft/exchange/webservices/data/EwsXmlReader.java
+++ src/main/java/microsoft/exchange/webservices/data/EwsXmlReader.java
@@ -708,19 +708,15 @@ class EwsXmlReader {
 	/**
 	 * Determines whether current element is a start element.
 	 * 
-	 * @param xmlNamespace
-	 *            the xml namespace
-	 * @param localName
-	 *            the local name
-	 * @return boolean
+	 * @param xmlNamespace  the xml namespace
+	 * @param localName     the local name
+	 * @return boolean      true for matching start element; false otherwise.
 	 */
 	public boolean isStartElement(XmlNamespace xmlNamespace, String localName) {
-		return (this.isStartElement())
-		&& (this.getLocalName().equals(localName))
-		&& ((this.getNamespacePrefix() == EwsUtilities
-				.getNamespacePrefix(xmlNamespace)) || (this
-						.getNamespaceUri() == EwsUtilities
-						.getNamespaceUri(xmlNamespace)));
+		return this.isStartElement()
+		&& this.getLocalName().equals(localName)
+		&& (this.getNamespacePrefix().equals(EwsUtilities.getNamespacePrefix(xmlNamespace)) ||
+			this.getNamespaceUri().equals(EwsUtilities.getNamespaceUri(xmlNamespace)));
 	}
 
 	/**
