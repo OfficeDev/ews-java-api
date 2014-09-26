2e2ec8bb13804c49d50472f9c2cea738bdce0948
Victor Boctor <vboctor@microsoft.com>
2014-09-26 11:20:56 -0700

    Fix #79: License confusion

--- src/main/java/microsoft/exchange/webservices/data/RemoveFromCalendar.java
+++ src/main/java/microsoft/exchange/webservices/data/RemoveFromCalendar.java
@@ -1,9 +1,13 @@
 /**************************************************************************
- * copyright file="RemoveFromCalendar.java" company="Microsoft"
- *     Copyright (c) Microsoft Corporation.  All rights reserved.
- * 
- * Defines the RemoveFromCalendar.java.
+ Exchange Web Services Java API
+ Copyright (c) Microsoft Corporation
+ All rights reserved.
+ MIT License
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  **************************************************************************/
+
 package microsoft.exchange.webservices.data;
 
 import java.util.List;
@@ -11,8 +15,6 @@ import java.util.List;
 /**
  * Represents a response object created to remove a calendar item from a meeting
  * cancellation.
- * 
- * 
  */
 @ServiceObjectDefinition(xmlElementName = XmlElementNames.RemoveItem, returnedByServer = false)
  class RemoveFromCalendar extends ServiceObject {
--- src/main/java/microsoft/exchange/webservices/data/ServiceRequestBase.java
+++ src/main/java/microsoft/exchange/webservices/data/ServiceRequestBase.java
@@ -1,9 +1,13 @@
 /**************************************************************************
- * copyright file="ServiceRequestBase.java" company="Microsoft"
- *     Copyright (c) Microsoft Corporation.  All rights reserved.
- * 
- * Defines the ServiceRequestBase.java.
+ Exchange Web Services Java API
+ Copyright (c) Microsoft Corporation
+ All rights reserved.
+ MIT License
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  **************************************************************************/
+
 package microsoft.exchange.webservices.data;
 
 import java.io.ByteArrayInputStream;
@@ -20,11 +24,8 @@ import java.util.zip.InflaterInputStream;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.ws.http.HTTPException;
 
-//import org.eclipse.ecf.core.util.AsyncResult;
-
 /**
  * Represents an abstract service request.
- * 
  */
 abstract class ServiceRequestBase {
 
