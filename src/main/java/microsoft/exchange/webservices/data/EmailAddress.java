2874be7a0981c75acba146fcef6e95b740ba5f3d
Amir Hossein Hodjati Pour <AmirHossein@users.noreply.github.com>
2014-11-11 11:03:21 +0330

    EmailAddress.equals() overloaded to check also SMTP addresses
    Method overloaded rather than overriding to show its function explicitly.

--- src/main/java/microsoft/exchange/webservices/data/EmailAddress.java
+++ src/main/java/microsoft/exchange/webservices/data/EmailAddress.java
@@ -384,4 +384,18 @@ public class EmailAddress extends ComplexProperty implements
 		return SmtpRoutingType;
 	}
 
+	/**
+	 * Determines whether a specific email address equals current SMTP address.
+	 *
+	 * @param address The email address to compare.
+	 * @return True if the address was not empty and equal to current SMTP address, false
+	 * otherwise.
+	 */
+	public boolean equals(String address) {
+		if (address != null && this.address != null) {
+			address = address.trim().toLowerCase();
+			return !address.isEmpty() && address.equals(this.address.trim().toLowerCase());
+		}
+		return false;
+	}
 }
