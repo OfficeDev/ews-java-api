f86846d52f7bffb4b20a4612d4d070081d8929a3
André Behrens (serious6) <andre.behrens@haw-hamburg.de>
2015-02-11 19:50:58 +0100

    license-header with checkstyle google-style
    - the license header was treated as a normal javadoc due to /** -> /*
    solves the problem
    - there should be a blank between license header and package

--- src/test/java/microsoft/exchange/webservices/data/BaseTest.java
+++ src/test/java/microsoft/exchange/webservices/data/BaseTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * The MIT License
  * Copyright (c) 2012 Microsoft Corporation
  *
@@ -20,6 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package microsoft.exchange.webservices.data;
 
 import org.junit.BeforeClass;
