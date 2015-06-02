756afa168a7a87283271ccb9f31955f46f1e86ba
Avrom <avromf@AVROMF-W7.andar.local>
2015-06-01 22:24:16 -0400

    Add Jaavdoc for ServiceRequestBase

--- src/main/java/microsoft/exchange/webservices/data/core/request/ServiceRequestBase.java
+++ src/main/java/microsoft/exchange/webservices/data/core/request/ServiceRequestBase.java
@@ -665,14 +665,23 @@ public abstract class ServiceRequestBase<T> {
     return buildEwsHttpWebRequest(request);
   }
 
+  /**
+   * Builds a HttpWebRequest object from a pooling connection manager for current service request
+   * with exception handling.
+   * <p>
+   * Used for subscriptions.
+   * </p>
+   * 
+   * @return A HttpWebRequest instance
+   * @throws Exception on error
+   */
   protected HttpWebRequest buildEwsHttpPoolingWebRequest() throws Exception {
-     HttpWebRequest request = service.prepareHttpPoolingWebRequest();
+    HttpWebRequest request = service.prepareHttpPoolingWebRequest();
     return buildEwsHttpWebRequest(request);
   }
 
-private HttpWebRequest buildEwsHttpWebRequest(HttpWebRequest request) throws Exception
-{
-	try {
+  private HttpWebRequest buildEwsHttpWebRequest(HttpWebRequest request) throws Exception {
+    try {
 
       service.traceHttpRequestHeaders(TraceFlags.EwsRequestHttpHeaders, request);
 
@@ -680,7 +689,8 @@ private HttpWebRequest buildEwsHttpWebRequest(HttpWebRequest request) throws Exc
 
       EwsServiceXmlWriter writer = new EwsServiceXmlWriter(service, requestStream);
 
-      boolean needSignature = service.getCredentials() != null && service.getCredentials().isNeedSignature();
+      boolean needSignature =
+          service.getCredentials() != null && service.getCredentials().isNeedSignature();
       writer.setRequireWSSecurityUtilityNamespace(needSignature);
 
       writeToXml(writer);
